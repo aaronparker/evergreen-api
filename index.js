9c4a7c96ea976a8370be29d37842f3d4be9cf9bc
Aaron Parker <aaron@stealthpuppy.com>
2022-06-01 08:37:43 +1000

    Update response data and codes

--- index.js
+++ index.js
@@ -4,19 +4,56 @@ const app = new restCfWorker()
 
 // Returns data for a specific app
 app.get("/app/:appId", async (req, res) => {
-  console.log("getApp")
   const word = req.params.appId;
-  console.log("Param: " + word)
+  console.log("get app: " + word + ".");
   let data = await EVERGREEN.get(word);
-  return res.send(JSON.parse(data));
+  return res.send(
+    JSON.parse(data),
+    { "content-type": "application/json" },
+    200
+  );
 });
 
 // Returns data for all supported apps
+const noApp = {
+  "Status": 1,
+  "Message": "Application not found. List all apps for valid application requests. Application names are case sensitive."
+}
+
 app.get("/apps", async (req, res) => {
-  console.log("getApps")
+  console.log("get all apps.")
   let data = await EVERGREEN.get("_AllApps");
-  return res.send(JSON.parse(data));
+  if (data === null) {
+    return res.send(
+      noApp,
+      { "content-type": "application/json" },
+      404
+    );
+  }
+  else {
+    return res.send(
+      JSON.parse(data),
+      { "content-type": "application/json" },
+      200
+    );
+  }
 });
+
+// Return data for /*
+const noData = {
+  "Status": 1,
+  "Message": "Method not found. Call /apps for available applications."
+}
+
+app.get('/*', async (req, res) => {
+  console.log(req.body);
+  return res.send(
+    noData,
+    { "content-type": "text/html" },
+    404
+  );
+});
+
 
 // Responder
 addEventListener('fetch', event => {
