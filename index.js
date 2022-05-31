ce411bfbc1e54bbf0f745b9d0067efacf2a87a43
Aaron Parker <aaron@stealthpuppy.com>
2022-05-31 18:09:15 +1000

    Update comments

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+// Uses the cloudflare-worker-rest-api package
 import restCfWorker from 'cloudflare-worker-rest-api'
 const app = new restCfWorker()
 
+// Returns data for a specific app
 app.get("/app/:appId", async (req, res) => {
   console.log("getApp")
   const word = req.params.appId;
@@ -9,12 +11,14 @@ app.get("/app/:appId", async (req, res) => {
   return res.send(JSON.parse(data));
 });
 
+// Returns data for all supported apps
 app.get("/apps", async (req, res) => {
   console.log("getApps")
   let data = await EVERGREEN.get("_AllApps");
   return res.send(JSON.parse(data));
 });
 
+// Responder
 addEventListener('fetch', event => {
   event.respondWith(app.handleRequest(event.request))
 })
