3232c3796a011b7e5b5931250581be7ca04fa360
Aaron Parker <aaron@stealthpuppy.com>
2022-06-04 11:49:41 +1000

    Adding support for environments

--- scripts/Push-Data.ps1
+++ scripts/Push-Data.ps1
@@ -6,3 +6,15 @@ foreach ($file in (Get-ChildItem -Path "*.json")) {
 Find-EvergreenApp | ConvertTo-Json | Out-File -FilePath "./AllApps.json" -Encoding "Utf8" -NoNewline
 wrangler kv:key put "_allapps" --path="./AllApps.json" --namespace-id=$id
 Remove-Item -Path "./AllApps.json"
+
+
+
+foreach ($file in (Get-ChildItem -Path "*.json")) {    
+    $id = "567d3c587d65448fa2e78986e623ee3a"
+    wrangler kv:key put $file.BaseName.ToLower() --path=$($file.FullName) --namespace-id=$id
+}
+
+Find-EvergreenApp | ConvertTo-Json | Out-File -FilePath "./AllApps.json" -Encoding "Utf8" -NoNewline
+wrangler kv:key put "_allapps" --path="./AllApps.json" --namespace-id=$id
+Remove-Item -Path "./AllApps.json"
+
